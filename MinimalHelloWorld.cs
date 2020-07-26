6fb1c66d6b8e753171fff04558ed54f336db7854
Isham Mohamed <IshamMohamed@users.noreply.github.com>
2020-07-26 20:40:19 +0800

    Update MinimalHelloWorld.cs

--- MinimalHelloWorld.cs
+++ MinimalHelloWorld.cs
@@ -1,2 +1,21 @@
-using System;
-Console.WriteLine("Hello world");
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
+
+CreateHostBuilder(args).Build().Run();
+
+IHostBuilder CreateHostBuilder(string[] args) =>
+	Host.CreateDefaultBuilder(args)
+		.ConfigureWebHostDefaults(webBuilder =>
+			webBuilder
+			.Configure(app =>
+			{
+				app.Run(context =>
+				{
+					return context.Response.WriteAsync("Hello world");
+				});
+			})
+		);
